1c94c21fc5f9a53954ef96cf4c8f5c63ca213df0
Michael Karén <michael.karen@gmail.com>
2019-02-12 14:26:59 +0100

    cache interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RetryInterceptor } from './interceptors/retry.interceptor';
+import { CacheInterceptor } from './interceptors/cache.interceptor';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, HttpClientModule, AppRoutingModule],
   providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: RetryInterceptor, multi: true }
+    //{ provide: HTTP_INTERCEPTORS, useClass: RetryInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: CacheInterceptor, multi: true }
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/interceptors/cache.interceptor.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpEvent,
+  HttpRequest,
+  HttpHandler,
+  HttpInterceptor,
+  HttpResponse
+} from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { tap, shareReplay } from 'rxjs/operators';
+
+@Injectable()
+export class CacheInterceptor implements HttpInterceptor {
+  private cache = new Map<string, any>();
+
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    if (request.method !== 'GET') {
+      return next.handle(request);
+    }
+
+    const cachedResponse = this.cache.get(request.url);
+    if (cachedResponse) {
+      return of(cachedResponse);
+    }
+
+    return next.handle(request).pipe(
+      tap(event => {
+        if (event instanceof HttpResponse) {
+          this.cache.set(request.url, event);
+        }
+      })
+    );
+  }
+}
